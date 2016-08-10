66e452edfab8b40bfe621425449b27406fe80c32
Mansi Patel <patelmansi7075@gmail.com>
2016-08-10 15:12:35 +0530

    Addition and Subtraction

--- Sum.c
+++ Sum.c
@@ -5,8 +5,11 @@ int main()
 	int a,b;
 	printf("Enter two numbers - ");
 	scanf("%d%d",&a,&b);
+	c=a;
 	printf("Sum of %d and %d is :",a,b);
 	a += b;
 	printf("%d\n",a);
-	
+	c-=b;
+	printf("Difference : %d",c);
+	return 0;
 }
